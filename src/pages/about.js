fb11d3adac28dd064046992782a4c9035481c71c
Andrew Tattersall <tattwo@live.com>
2021-04-11 19:01:39 +0100

    added bold

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,19 +20,22 @@ const About = () => {
         <p>
           React.js is an open-source JavaScript library that is used for
           building user interfaces specifically for single-page applications.
-          It’s used for handling the view layer for web and mobile apps. React
-          also allows us to create reusable UI components. React was first
-          created by Jordan Walke, a software engineer working for Facebook.
-          React first deployed on Facebook’s newsfeed in 2011 and on
-          Instagram.com in 2012.
+          It’s used for handling the view layer for web and mobile apps.{" "}
+          <strong>React also allows us to create reusable UI components</strong>
+          . React was first created by Jordan Walke, a software engineer working
+          for Facebook. React first deployed on Facebook’s newsfeed in 2011 and
+          on Instagram.com in 2012.
         </p>
         <p>
           As a junior developer, I held off learning a JS framework for months
-          and just focused on learning the fundamentals well. I think React is a
-          good move for now, but who knows, maybe Vue will overtake React or
-          maybe even Angular will make a comeback. But in particular at the
-          start of your coding journey, a framework will allow you build bigger
-          and more ambitious projects and keep you motivated.
+          and just focused on learning the fundamentals well.{" "}
+          <strong>
+            I think React is a good investment for now, but who knows, maybe Vue
+            will overtake React or maybe even Angular will make a comeback
+          </strong>
+          . But in particular at the start of your coding journey, a framework
+          will allow you build bigger and more ambitious projects and keep you
+          motivated.
         </p>
         <img src={meme2} alt="React meme" />
       </Layout>
